59451f5597895670ef30d8fa6e9a9695bc587cc1
ShubhaM <81143367+shubham-1319@users.noreply.github.com>
2023-12-13 23:02:30 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+// created by shubh
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Writing numbers from 1 to 10
+    cout << "Numbers 1 to 10:" << endl;
+    for (int i = 1; i <= 10; ++i) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    // Reversing the numbers
+    cout << "Reversed numbers:" << endl;
+    for (int i = 10; i >= 1; --i) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
